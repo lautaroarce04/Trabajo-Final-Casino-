e90926bf18de7f65fc710c81ce6ec5bcd9e72ed3
Lautaro <lautaroarce26@gmail.com>
2025-06-07 23:45:09 -0300

    dados aleatorios

--- Dados.ts
+++ Dados.ts
@@ -1,8 +1,5 @@
 import { JuegoBase } from "./JuegoBase";
 import chalk from "chalk";
-import promptSync from "prompt-sync";
-
-const prompt = promptSync();
 
 export class Dados extends JuegoBase {
   constructor() {
@@ -13,13 +10,6 @@ export class Dados extends JuegoBase {
     try {
       this.validarApuesta(apuesta);
 
-      const numeroStr = prompt("Elegí un número del 2 al 12: ")!;
-      const numero = parseInt(numeroStr);
-
-      if (!Number.isInteger(numero) || numero < 2 || numero > 12) {
-        throw new Error("Número inválido. Debe ser un entero entre 2 y 12.");
-      }
-
       const dado1 = Math.floor(Math.random() * 6) + 1;
       const dado2 = Math.floor(Math.random() * 6) + 1;
       const suma = dado1 + dado2;
@@ -29,10 +19,7 @@ export class Dados extends JuegoBase {
       console.log(chalk.blue(`Dado 2: [${dado2}]`));
       console.log(chalk.white(`Suma: ${suma}`));
 
-      if (suma === numero) {
-        console.log(chalk.green("¡Acertaste el número! Ganás 6x la apuesta 🎉"));
-        return apuesta * 6;
-      } else if (suma === 7 || suma === 11) {
+      if (suma === 7 || suma === 11) {
         console.log(chalk.green("¡Ganaste! Suma mágica 🎉"));
         return apuesta * 5;
       } else {
@@ -41,7 +28,6 @@ export class Dados extends JuegoBase {
       }
 
     } catch (error) {
-      // Muestra solo el mensaje, no la stack trace
       console.log(chalk.red("Error en Dados: "), (error as Error).message);
       return 0;
     }
